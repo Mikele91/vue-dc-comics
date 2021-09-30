6673c13e6f708a695511c9db056301af007ba279
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-30 13:09:03 +0200

    ultime modifiche

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,64 @@
    <footer>
       <div class="container">
          <div class="top-footer">
-         <h2>Sono il Footer</h2>
+            <div class="list">
+               <div class="col-list">
+               <div class="section-list">
+
+               <h3>Titolo List</h3>
+            <ul>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+
+            </ul>
+               </div>
+                 <div class="section-list">
+
+               <h3>Titolo List</h3>
+            <ul>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+
+            </ul>
+               </div>
+
+               </div>
+                 <div class="col-list">
+               <div class="section-list">
+
+               <h3>Titolo List</h3>
+            <ul>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+
+            </ul>
+               </div>
+                 <div class="section-list">
+
+               <h3>Titolo List</h3>
+            <ul>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+               <li><a href="">link 1</a></li>
+
+            </ul>
+               </div>
+
+               </div>
+
+            </div>
+            
          <img src="@/assets/img/dc-logo-bg.png" alt="">
          </div>
          <div class="bottom-footer">
@@ -21,6 +78,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import"src/assets/style/variables.scss";
+
    footer{
       background-image: url("../assets/img/footer-bg.jpg") ;
       background-size: cover;
@@ -29,6 +88,22 @@ export default {
       .top-footer{
          display: flex;
          justify-content: space-between;
+      }
+      .list{
+               display: flex;
+               color: white;
+            .col-list{
+               margin: 0 1.25rem
+            }  
+         
+         li{
+            @include list;
+   
+            a{
+               color: white;
+               @include list;
+            }
+         }
       }
    }
 </style>
